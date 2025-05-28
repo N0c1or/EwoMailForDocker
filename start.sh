92a72fa4b9dec542f493779432d3fa435cad004d
N0c1or <126218169+N0c1or@users.noreply.github.com>
2025-05-28 15:47:23 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 docker build -t ewomail:v1 .;
 docker run -itd --name ewomail --restart=always --privileged=true -p 7000:7000 -p 7010:7010 -p 25:25 -p 143:143 -p 993:993 -p 995:995 -p 587:587 -p 110:110 -p 465:465 -p 8020:8020 -p 8000:8000 -p 8010:8010 ewomail:v1;
-docker cp ./CentOS-Base.repo ewomail:/root/install/CentOS-Base.repo
-docker cp ./epel.repo ewomail:/root/install/epel.repo
+docker cp ./CentOS-Base.repo ewomail:/etc/yum.repos.d/CentOS-Base.repo
+docker cp ./epel.repo ewomail:/etc/yum.repos.d/epel.repo
 docker exec -d --privileged ewomail /bin/sh -c 'cd /root/install/ && sh start.sh xxx.com';
 docker exec -d --privileged ewomail /bin/sh -c "echo '127.0.0.1   mail.xxx.com smtp.xxx.com imap.xxx.com' >> /etc/hosts";
